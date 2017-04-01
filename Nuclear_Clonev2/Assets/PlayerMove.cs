3062300345a712919c94912a1ead06ab55fed3c8
Remy <emry.rosnik@gmail.com>
2017-03-31 17:35:32 -0700

    Added Files setup in Unity for Git
    Readded files after changing some project settings

---
+++ Nuclear_Clonev2/Assets/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+    public float movex;
+    public float movey;
+    Animator anim;
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate () {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        movex = Input.GetAxis("Horizontal");
+        movey = Input.GetAxis("Vertical");
+        rb2d.velocity = new Vector2(movex * speed, movey * speed);
+    }
+}
